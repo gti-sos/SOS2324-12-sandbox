735858ec73bd0615dc4d30b555ce34eb7e589167
josmarsua <josemartinezsuarez0@gmail.com>
2024-02-25 13:31:31 +0100

    L04

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let express = require("express");
 
 let app = express();
 
-const PORT = 10000; //process.env.PORT; para leer la variable de entorno PORT (hay que definirla en cmd)
+const PORT = (process.env.PORT || 10000); //process.env.PORT; para leer la variable de entorno PORT (hay que definirla en cmd)
 
-app.get("/", (req,res) => {
+app.use("/",express.static("./public"));
+
+app.get("/hola", (req,res) => {
     res.send("<html><body><h1>Hola mundo!</h1></body></html>")
 });
 
